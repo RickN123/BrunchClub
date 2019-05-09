388805b607033214276079da76f92601e35dac9c
mattwills09 <mattwills09@gmail.com>
2019-05-09 10:46:39 -0400

    Pre-coded parties table

--- 04-Stu_Project-Starter/Unsolved/schema.sql
+++ 04-Stu_Project-Starter/Unsolved/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE parties
 	id int NOT NULL AUTO_INCREMENT,
 	venue varchar(255) NOT NULL,
 	theme varchar(255) NOT NULL,
-    date_time DATETIME(100) NOT NULL,
+    date_time DATETIME(6) NOT NULL,
     specials VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL,
     neighborhood VARCHAR(155) NOT NULL,
